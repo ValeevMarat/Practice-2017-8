0c57a498fa6f933af61406063efe60124ec9b2ea
Marat <user@LAPTOP-7JKVIGGR>
2017-07-09 17:16:43 +0500

    Код написан и прокомментирован. Присутствует описание задания и ссылка на него. Защиты от "дурака" нет. Генератор тестов в следующем коммите

--- Practice 2017 8/Program.cs
+++ Practice 2017 8/Program.cs
@@ -8,8 +8,47 @@ namespace Practice_2017_8
 {
     class Program
     {
+        static bool IsTree(bool[,] adjacencyMatr)
+        {                   // Матрица смежности
+            int edges = 0, n = adjacencyMatr.GetLength(0);      // edges - количество рёбер и n - размерность матрицы и количество вершин
+
+            for (int i = 0; i < n; i++)                         // Проходимся повсем рядам
+            {
+                bool onlyZeroes = true;                         // Маркер того, что в ряду нет единиц
+                for (int j = 0; j < n; j++)                     // По всем столбцам
+                {
+                    if (!adjacencyMatr[i, j]) continue;         // Пропускаем нули
+                    onlyZeroes = false;                         // Если 1, то меняем маркер и прибавляем рёбра
+                    edges++;
+                }
+                if (onlyZeroes | edges / 2 >= n) return false;  // Если в ряду одни нули (значит граф не связный) или кол-во рёбер больше n-1 (у деревьев кол-во рёбер на 1 меньше, чем вершин), то граф не является деревом
+            }
+            return true;
+        }    // Проверяет является ли граф заданный матрицей смежности деревом
+
+        static bool[,] ReadAdjacencyMatr()
+        {
+            Console.Write("Введите размерность матрицы смежности: ");
+            int n = int.Parse(Console.ReadLine());
+
+            bool[,] adjacencyMatr = new bool[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите " + (i + 1) + "-ую строку элементов (через пробел, единицы или нули):");
+                string row = Console.ReadLine();
+                for (int j = 0; j < n; j++)
+                    adjacencyMatr[i, j] = int.Parse(row.Split(' ')[j]) == 1;
+            }
+            return adjacencyMatr;
+        }           // Ввод матрицы смежности вручную
+
         static void Main(string[] args)
         {
+            Console.WriteLine(IsTree(ReadAdjacencyMatr()) ? "Заданный граф является деревом"
+                                                          : "Заданный граф не является деревом");
+            
+            Console.ReadKey();
         }
     }
 }
